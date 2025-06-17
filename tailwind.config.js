ef4eaea29195e57899d4dbc6df7ada63a3a0355f
Yash Sharma <yashsharma0492@gmail.com>
2025-06-17 22:13:37 +0530

    added nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["*"],
   theme: {
+    fontWeight:
+    {
+      "ssbold":"500"
+    },
     extend: {},
+    screens:
+    {
+      'vsmall':{'max':'370px'},
+      'small':{'max':'1030px'}
+    }
   },
   plugins: [],
 }
